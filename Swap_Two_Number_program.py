a89e014783e0c264a06fb23107595d2d7114c505
pratham20002 <satputepratham46@gmail.com>
2020-08-10 13:49:08 +0530

    swaping two numbers

---
+++ Swap_Two_Number_program.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Your first number: ",a)
+print("Your second numer: ",b)
+t = a
+a = b
+b = t
+print("Your new first number:" , a)
+print("Your new second number:" , b)
